0cf6d241e9652287f1e8b8a5329f59af04d5c24e
sadhanachandel99 <168443062+sadhanachandel99@users.noreply.github.com>
2025-02-02 23:50:59 -0500

    added javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Form Submission Alert
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+  });
